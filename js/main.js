7222e14672625d73aa10f3ab20aa67957b1d68b9
Margarita Bartholomew <margarita.veracruz@gmail.com>
2017-06-12 22:56:00 -0400

    adding back to top button on scroll

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
 $(document).ready(function() {
 
+  $('#back-to-top').css('opacity','0');
+  $(window).scroll(function(){
+
+    if ($(this).scrollTop()>150){
+        $('#back-to-top').css('opacity','1').css('transition','0.3s ease-in-out');
+        // $('a').css('font-size', '98%').css('transition','0.3s ease-in-out');
+      }else if ($(this).scrollTop()>100){
+        $('#back-to-top').css('opacity','.75').css('transition','0.3s ease-in-out');
+      }else if ($(this).scrollTop()>60){
+        $('#back-to-top').css('opacity','.5').css('transition','0.3s ease-in-out');
+      }else{
+        $('#back-to-top').css('opacity','0').css('transition','0.3s ease-in-out');
+        // $('a').css('font-size', '100%').css('transition','0.3s ease-in-out');
+      }
+  });
+
+
   //animate scroll to dofferent sections
 
   var aboutLink    = $('#about-link');
@@ -34,7 +51,7 @@ $(document).ready(function() {
     $("#skill-show i").toggleClass('fa-caret-down fa-caret-up');
   });
 
- // 
+ //
  // $('#rendezyou').on('mouseenter', function(){
  //   $('#rendezyou').attr("src", "assets/margarita.jpg");
  // })
